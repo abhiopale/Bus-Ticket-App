68714b3bbd4709ea850e37f27026d15f5b1cfe5d
abhiopale@gmail.com <abhiopale@gmail.com>
2022-04-21 18:12:14 +0530

    changes in the Ticket BookingAPI
    Signed-off-by: abhiopale@gmail.com <abhiopale@gmail.com>

--- server/db/model/bus.js
+++ server/db/model/bus.js
@@ -1,43 +1,43 @@
-const date = require('@hapi/joi/lib/types/date');
-const express = require('express');
-const { number, string, boolean } = require('joi');
+const date = require("@hapi/joi/lib/types/date");
+const express = require("express");
+const { number, string, boolean, object } = require("joi");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const busSchema= new mongoose.Schema({
-    name:{
-        type:String,
-        minlength:3,
-        trim:true,
-        required:true,
-    },
-    number:{
-        type:String,
-        required:true,
-        minlength:3,
-        trim:true,
-    },
-    rate:{
-        type:Number,
-        required:true,
-    },
-    date:{
-        type:String
-    },
-    time:{
-        type:String
-    },
-    noOfSeats:{
-        type:Number,
-        required:true
-    },
-    arrival:{
-        type:String
-    },
-    destiny:{
-        type:String
-    }
-})
+const busSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    trim: true,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true,
+  },
+  rate: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+  seats: {
+    type: Object,
+    required: true,
+  },
+  arrival: {
+    type: String,
+  },
+  destiny: {
+    type: String,
+  },
+});
 
-const bus = new mongoose.model("bus",busSchema);
-module.exports = bus
+const bus = new mongoose.model("bus", busSchema);
+module.exports = bus;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,263 +1,319 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config()
+require("dotenv").config();
 
 app.use(express.json());
 
-const validate = require('validator')
+const validate = require("validator");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const router = new express.Router();
 
-const Joi = require('@hapi/joi')
+const Joi = require("@hapi/joi");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const User = require('../db/model/user');
+const User = require("../db/model/user");
 
-const Admin = require('../db/model/admin');
+const Admin = require("../db/model/admin");
 
-const Bus = require('../db/model/bus');
+const Bus = require("../db/model/bus");
 
-const Ticket = require('../db/model/ticket');
+const Ticket = require("../db/model/ticket");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const bus = require("../db/model/bus");
+const values = require("@hapi/joi/lib/values");
 
 const jsonParser = bodyParser.json();
 
-function userAuth (req,res,next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader.split(' ')[1]
-    if (token==null)  return res.status(500).json({
-        status : "Error",
-        result : "Access Denied"
-        })
+function userAuth(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader.split(" ")[1];
+  if (token == null)
+    return res.status(500).json({
+      status: "Error",
+      result: "Access Denied",
+    });
 
-    jwt.verify(token,process.env.USER_KEY,(err, email)=>{
-        if (err) return res.status(500).json({
-            status : "Error",
-            result : "Access Denied"
-        })
-        req.email = email
-    })
-    next()
+  jwt.verify(token, process.env.USER_KEY, (err, email) => {
+    if (err)
+      return res.status(500).json({
+        status: "Error",
+        result: "Access Denied",
+      });
+    req.email = email;
+  });
+  next();
 }
 
-router.post('/user/login',jsonParser, async ( req, res) =>{
-    try{
-        let Email = req.body.email
-        let userDetails =  await User.findOne({email:Email});
-        if (userDetails){
-            const passCheck = await bcrypt.compare(req.body.password,userDetails.password);
-            
-            if (passCheck==true){
-                    const token = jwt.sign(Email,process.env.USER_KEY)
-                    res.json({
-                    status : "Success",
-                    result : {
-                        "token" : token,
-                    }
-                    })
-            }
-            else{res.status(500).json({
-                status : "Error",
-                result : "Incorrect Login Details"
-                })}
+router.post("/user/login", jsonParser, async (req, res) => {
+  try {
+    let Email = req.body.email;
+    let userDetails = await User.findOne({ email: Email });
+    if (userDetails) {
+      const passCheck = await bcrypt.compare(
+        req.body.password,
+        userDetails.password
+      );
 
-        }else{res.status(500).json({
-            status : "Error",
-            result : "Incorrect Login Details"
-            })}
-
-    }catch (e) {res.status(500).json({
-        status : "Error",
-        result : {
-            error : e
-        }
-        })}
-})
-
-router.post('/user/signup', jsonParser, async ( req, res)=>{
-    let {firstName, lastName, number, email, password} = req.body
-    try{
-        const existingUser = await User.findOne({email});
-        if (existingUser){
-            res.status(500).json({
-                status : "Error",
-                result : "Email already exists"
-            })}
-        
-        const existingNumber = await User.findOne({number});
-        if (existingNumber){
-            res.status(500).json({
-                status : "Error",
-                result : "Number already exists"
-            })
-        }
-       
-        // this will be implemented if the email in not register with the data base
-        else {
-            password = await bcrypt.hash(password,12);
-                let user = new User({
-                    firstName,
-                    lastName,
-                    number,
-                    email,
-                    isAdmin:false,
-                    password,
-                })
-                user.save();
-                res.status(200).json({
-                status : "Success",
-                result : "Successfully logged In"
-                })
-        }
-    }catch (e) {res.json({
-        status : "error",
-        result : "Please add the correct info"
-    })}
-})
-
-router.post('/user/bus/search',userAuth, jsonParser, async ( req, res)=>{
-    let {from, to,date} = req.body
-    try {
-        const busList = await Bus.find({arrival:from,destiny:to,date})
-        if (busList.length==0) {res.status(500).json({
-            status : "error",
-            result : "Sorry no Buses available"
-        })}
-        else{
-            res.status(500).json({
-                status : "Success",
-                result : busList
-            })
-        }
-
-    } catch (error) {
-        {res.status(500).json({
-            status : "error",
-            result : "Please fill in the appropriate details."
-        })}
-    }
-})
-
-router.post('/user/bus/booktickets',userAuth, jsonParser, async ( req, res)=>{
-    let email = req.email;
-    const {number,from,to,date,seatNo,modeOfPayment,passengerName} = req.body;
-    try {
-        let ticketList = [];
-        const busExist = await Bus.find({arrival:from,destiny:to,date,number})
-        let existingUser = await User.find({email});
-        if(!busExist){
-            res.status(500).json({
-                status : "Error",
-                result : "sorry no bus available on the given date"
-            })
-        }
-        else if (seatNo.length != passengerName.length){
-            res.status(500).json({
-                status : "Error",
-                result : "Please add the passenger Details"
-            })}
-        else if(seatNo.length > await Ticket.count({busNumber:number,isBooked:false})){
-            res.status(500).json({
-                status : "Error",
-                result : "Tickets not available"
-            })}
-            else{
-                let count = 0
-                for ( i=0 ;i < seatNo.length;i++){
-                    let seat = seatNo[i]
-                    if (await Ticket.findOne({seatNo:seat,busNumber:number,isBooked:false})) {
-                        count +=1
-                    }
-                }
-                if (count ==seatNo.length){
-                    for ( i=0 ;i < seatNo.length;i++){
-                        let seat = seatNo[i]
-                        let ticket = await Ticket.findOneAndUpdate({seatNo:seat,busNumber:number,isBooked:false},{$set:{customerName:passengerName[i],customerEmail:email,isBooked:true,modeOfPayment:modeOfPayment}});
-                        ticketList.push(ticket)
-                        }
-                        res.status(200).json({
-                            status : "Success",
-                            result : {
-                                message : "Ticket is Booked",
-                                ticketList : ticketList
-                            }
-                        })
-                }
-                else {
-                    res.status(500).json({
-                        status : "Error",
-                        result : "Tickets are already booked please check the availibility once"
-                    })
-                }
-                }
-            
-        
-        
-    } catch (error) {
-        {res.status(500).json({
-            status : "Error",
-            result : "Please fill in the appropriate details."
-        })}
-    }
-})
-
-router.post('/user/bus/ticket',userAuth, jsonParser, async ( req, res)=>{
-    let {number} = req.body;
-    try {
-        let busExist = await Bus.findOne({number})
-        if (!busExist) {res.status(500).json({
-            status : "Error",
-            result : "Please fill in the appropriate details."})}
-        else {
-            const ticketList = await Ticket.find({busNumber:number,isBooked:false})
-            newTicketList = [];
-            for ( i=0 ;i < ticketList.length;i++){
-                let ticket = ticketList[i]
-                newTicketList.push(ticket.seatNo)   
-            }
-            if (newTicketList.length ==0){
-                res.status(500).json({
-                    status : "Error",
-                    result : "No tickets Available"
-                })}
-                else{
-                    res.status(500).json({
-                        status : "Success",
-                        result : newTicketList
-                    })
-            }
-        }
-    } catch (error) {
-        {res.status(500).json({
-            status : "Error",
-            result : "Please fill in the appropriate details."
-        })}
-    }
-})
-
-router.get('/user/ticket', userAuth,jsonParser, async ( req, res)=>{
-    let email = req.email;
-    try {
-        const tickets = await Ticket.find({customerEmail:email})
+      if (passCheck == true) {
+        const token = jwt.sign(Email, process.env.USER_KEY);
         res.json({
-            status : "Success",
-            result : tickets
-        })
-        
-    } catch (error) {
-        {res.status(500).json({
-            status : "Error",
-            result : "Please fill in the appropriate details."
-        })}  
+          status: "Success",
+          result: {
+            token: token,
+          },
+        });
+      } else {
+        res.status(500).json({
+          status: "Error",
+          result: "Incorrect Login Details",
+        });
+      }
+    } else {
+      res.status(500).json({
+        status: "Error",
+        result: "Incorrect Login Details",
+      });
     }
-})
+  } catch (e) {
+    res.status(500).json({
+      status: "Error",
+      result: {
+        error: e,
+      },
+    });
+  }
+});
+
+router.post("/user/signup", jsonParser, async (req, res) => {
+  let { firstName, lastName, number, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(500).json({
+        status: "Error",
+        result: "Email already exists",
+      });
+    }
+
+    const existingNumber = await User.findOne({ number });
+    if (existingNumber) {
+      res.status(500).json({
+        status: "Error",
+        result: "Number already exists",
+      });
+    }
+
+    // this will be implemented if the email in not register with the data base
+    else {
+      password = await bcrypt.hash(password, 12);
+      let user = new User({
+        firstName,
+        lastName,
+        number,
+        email,
+        isAdmin: false,
+        password,
+      });
+      user.save();
+      res.status(200).json({
+        status: "Success",
+        result: "Successfully logged In",
+      });
+    }
+  } catch (e) {
+    res.json({
+      status: "error",
+      result: "Please add the correct info",
+    });
+  }
+});
+
+router.post("/user/bus/search", userAuth, jsonParser, async (req, res) => {
+  let { from, to, date } = req.body;
+  try {
+    const busList = await Bus.find({ arrival: from, destiny: to, date });
+    if (busList.length == 0) {
+      res.status(500).json({
+        status: "error",
+        result: "Sorry no Buses available",
+      });
+    } else {
+      res.status(500).json({
+        status: "Success",
+        result: busList,
+      });
+    }
+  } catch (error) {
+    {
+      res.status(500).json({
+        status: "error",
+        result: "Please fill in the appropriate details.",
+      });
+    }
+  }
+});
+
+router.post("/user/bus/booktickets", userAuth, jsonParser, async (req, res) => {
+  let email = req.email;
+  const {
+    number,
+    from,
+    to,
+    date,
+    seatNo,
+    modeOfPayment,
+    passengerName,
+  } = req.body;
+  try {
+    let ticketList = [];
+    let busExist = await Bus.findOne({
+      arrival: from,
+      destiny: to,
+      date,
+      number,
+    });
+    if (!busExist) {
+      res.status(500).json({
+        status: "Error",
+        result: "sorry no bus available on the given date",
+      });
+    } else if (seatNo.length != passengerName.length) {
+      res.status(500).json({
+        status: "Error",
+        result: "Please add the passenger Details",
+      });
+    } else {
+      let count = 0;
+      bookedSeats = [];
+      seatNo.forEach((seat) => {
+        seat = seat.toString();
+        let actaulSeat = busExist.seats[seat];
+        if (actaulSeat.isBooked == true) {
+          bookedSeats.push(seat);
+        } else {
+          count += 1;
+        }
+      });
+      if (count == seatNo.length) {
+        let newSeatCollection = {};
+        ticketList = [];
+        for (i = 0; i < seatNo.length; i++) {
+          let ticket = new Ticket({
+            seatNo: seatNo[i],
+            customerName: passengerName[i],
+            customerEmail: email,
+            isBooked: true,
+            busNumber: number,
+            rate: busExist.rate,
+            date,
+            time: busExist.time,
+            arrival: from,
+            destiny: to,
+            modeOfPayment,
+          });
+          newSeatCollection[seatNo[i]] = {
+            isBooked: true,
+            ticket: ticket._id,
+          };
+          ticket.save();
+          ticketList.push(ticket);
+        }
+        let keys = Object.keys(newSeatCollection);
+        keys.map((x) => {
+          busExist.seats[x] = newSeatCollection[x];
+        });
+        let newSeats = busExist.seats;
+        await Bus.findOneAndUpdate(
+          { number, date },
+          { $set: { seats: newSeats } }
+        );
+        res.json({
+          status: "Success",
+          result: "Tickets are booked",
+          ticketList: ticketList,
+        });
+      } else {
+        res.json({
+          status: "Error",
+          result: "seat numbers are already booked are booked",
+          bookedSeats: bookedSeats,
+        });
+      }
+    }
+  } catch (error) {
+    {
+      res.status(500).json({
+        status: "Error",
+        result: "Please fill in the appropriate details.",
+      });
+    }
+  }
+});
+
+router.post("/user/bus/ticket", userAuth, jsonParser, async (req, res) => {
+  let { number, date } = req.body;
+  try {
+    let busExist = await Bus.findOne({ number, date });
+    if (!busExist) {
+      res.status(500).json({
+        status: "Error",
+        result: "Please fill in the appropriate details.",
+      });
+    } else {
+      availableTicketList = [];
+      count = 0;
+      Object.values(busExist.seats).forEach((seat) => {
+        count += 1;
+        if (seat.isBooked == false) {
+          availableTicketList.push(count);
+        }
+      });
+      if (availableTicketList.length == 0) {
+        res.status(500).json({
+          status: "Error",
+          result: "No tickets Available",
+        });
+      } else {
+        res.status(500).json({
+          status: "Success",
+          result: availableTicketList,
+        });
+      }
+    }
+  } catch (error) {
+    {
+      res.status(500).json({
+        status: "Error",
+        result: "Please fill in the appropriate details.",
+      });
+    }
+  }
+});
+
+router.get("/user/ticket", userAuth, jsonParser, async (req, res) => {
+  let email = req.email;
+  try {
+    const tickets = await Ticket.find({ customerEmail: email });
+    res.json({
+      status: "Success",
+      result: tickets,
+    });
+  } catch (error) {
+    {
+      res.status(500).json({
+        status: "Error",
+        result: "Please fill in the appropriate details.",
+      });
+    }
+  }
+});
 
 module.exports = router;
